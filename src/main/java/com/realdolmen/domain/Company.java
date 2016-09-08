bf0b6008fceafd480fcc6709abe7e61252207bf9
Ziggy Streulens <Ziggy.Streulens@realdolmen.com>
2016-09-08 10:11:58 +0200

    Company login added

---
+++ src/main/java/com/realdolmen/domain/Company.java
@@ -0,0 +1,36 @@
+package com.realdolmen.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+import org.mindrot.BCrypt;
+
+@Entity
+@NamedQuery(name = "Company.findByName", query = "SELECT c FROM Company c WHERE c.name = :name")
+public class Company {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String name;
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		String hashed = BCrypt.hashpw(password, BCrypt.gensalt());
+		this.password = hashed;
+	}
+
+}
---
+++ src/main/java/com/realdolmen/repository/CompanyRepository.java
@@ -0,0 +1,31 @@
+package com.realdolmen.repository;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.realdolmen.domain.Company;
+
+@Stateless
+public class CompanyRepository {
+	@PersistenceContext
+	EntityManager entityManager;
+	
+	public Company createCompany(Company company){
+		entityManager.persist(company);
+		return company;
+	}
+	
+	public Company updateCompany(Company company){
+		entityManager.merge(company);
+		return company;
+	}
+	
+	public Company findById(Integer id){
+		return entityManager.find(Company.class, id);
+	}
+	
+	public Company findByName(String name){
+		return entityManager.createNamedQuery("Company.findByName", Company.class).setParameter("name", name).getSingleResult();
+	}
+}
